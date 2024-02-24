56258150a0d5cb37041b61180de2642a59105ed4
Abhishek Chauhan <ashu.topcoder@gmail.com>
2024-02-24 12:53:16 +0530

    Programs

---
+++ String Programs/CountCharacter.java
@@ -0,0 +1,14 @@
+//Count Character In String
+import java.util.Scanner;
+public class CountCharacter{
+public static void main(String args[]){
+	Scanner sc =new Scanner(System.in);
+	String str =sc.next();
+	int count=0;
+	for(int i=0;i<str.length();i++){
+		if((str.charAt(i)!=' ')&&(str.charAt(i)!='@')&&(str.charAt(i)!='$')&&(str.charAt(i)!='.'))
+			count++;
+	}
+	System.out.println("Character In A String Is : "+count);
+	}
+}
