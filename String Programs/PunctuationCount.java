bc6c6e4a543b23c43fed0b7f7da4545de706c7aa
Abhishek Chauhan <ashu.topcoder@gmail.com>
2024-02-24 13:15:25 +0530

    Programs

--- String Programs/PunctuationCount.java
+++ String Programs/PunctuationCount.java
@@ -1,15 +1,15 @@
-//Count Character In String
+//Count Punctuation In String
 import java.util.Scanner;
-public class CountCharacter{
+public class PunctuationCount{
 public static void main(String args[]){
 	Scanner sc =new Scanner(System.in);
 	System.out.print("Enter A String : ");
 	String str =sc.next();
 	int count=0;
 	for(int i=0;i<str.length();i++){
-		if((str.charAt(i)!=' ')&&(str.charAt(i)!='@')&&(str.charAt(i)!='$')&&(str.charAt(i)!='.'))
+		if(!((str.charAt(i)!=' ')&&(str.charAt(i)!='@')&&(str.charAt(i)!='$')&&(str.charAt(i)!='.')))
 			count++;
 	}
-	System.out.println("Character In A String Is : "+count);
+	System.out.println("Punctuation In A String Is : "+count);
 	}
 }
