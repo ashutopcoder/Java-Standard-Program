1e0d87b097fb36ed551bed73da5b6fc8b50e3c35
Abhishek <ashu.topcoder@gmail.com>
2024-12-24 20:44:09 +0530

    Updated

--- NUmberProgram/Factorial.java
+++ NUmberProgram/Factorial.java
@@ -1,18 +1,15 @@
-package com.demo;
-
 import java.util.Scanner;
 
-public class Factorial {
-	public static void main(String [] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter A Number : ");
-		int num=sc.nextInt();
-		int a=1;
-		for(int i=num;i>=1;i--) {
-			a=a*i;
+class Factorial{
+	
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter A Number Which Number Factorial You Find : ");
+		int num=sc.nextInt();;
+		int fact=1;
+		for(int i=num;i>0;i--){
+			fact=fact*i;
 		}
-		System.out.println("Factorial Of "+num+" is "+a);
-		
+		System.out.println("Facorial Of A Number Is : "+fact);
 	}
-
 }
